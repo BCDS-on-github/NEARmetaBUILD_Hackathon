629b1fd443212477ceba0b13ff0d53e9f57d63ff
BCDS-on-github <99432179+BCDS-on-github@users.noreply.github.com>
2022-02-21 09:56:15 -0700

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -3,9 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Mysterious bullshit sorcery goes here
+import { Buffer } from "buffer";
+global.Buffer = Buffer;
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC9TMEu9INyvSi8WunDMGgrVX8D88sfWLc",
+  authDomain: "nearmetabuild-hackathon.firebaseapp.com",
+  projectId: "nearmetabuild-hackathon",
+  storageBucket: "nearmetabuild-hackathon.appspot.com",
+  messagingSenderId: "459108344073",
+  appId: "1:459108344073:web:f674c06c60b07fb66573d8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
+  	<style>{'body { background-color: #121212; }'}</style>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
